408f3e661fa07a4d9d528f930cebd2e700911c7b
Razmik Poghosyan <razmikarm@gmail.com>
2024-12-28 00:46:30 +0400

    Partially added testing components

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+# TODO: Intialize tests for pytest
+
+def my_func():
+    return [1, 2, 3]
+
+def test_pytest():
+    expected_values = [1, 2, 3]
+    actual_values = my_func()
+    assert expected_values == actual_values
